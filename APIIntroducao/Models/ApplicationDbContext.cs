ad235fa195ba3a1f8e8d2e293fc60959747be127
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-01-07 11:38:49 -0300

    Added a constructor to the DbContext file

--- APIIntroducao/Models/ApplicationDbContext.cs
+++ APIIntroducao/Models/ApplicationDbContext.cs
@@ -4,6 +4,12 @@ namespace APIIntroducao.Models
 {
     public class ApplicationDbContext : DbContext
     {
+        // Construtor necessário para funcionar o servido no arquivo Startup
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+
         /*
             A classe ApplicationDbContext, deve receber o nome de todos os modelos criados,
             o DbSet recebe o nome do modelo, você passa o nome que quer dar para a tabela
