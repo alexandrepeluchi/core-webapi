25a7643b85986fe598cca77265e91a1267d9dcb3
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-01-07 11:42:01 -0300

    Updated the Categories controller, added a route, response format constraint and an http get method

--- APIIntroducao/Controllers/CategoriesController.cs
+++ APIIntroducao/Controllers/CategoriesController.cs
@@ -1,9 +1,26 @@
+using System.Collections.Generic;
+using System.Linq;
+using APIIntroducao.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace APIIntroducao.Controllers
+namespace APIIntroducao.Controllers 
 {
-    public class CategoriesController : Controller
+    // Restringe o formato de resposta e escolha de uma rota
+    [Produces("application/json")]
+    [Route("api/categories")]
+    public class CategoriesController : Controller 
     {
-        
+        public readonly ApplicationDbContext context;
+        public CategoriesController (ApplicationDbContext context) 
+        {
+            this.context = context;
+        }
+
+        // Método chamado via API, por isso a marcação HttpGet
+        [HttpGet]
+        public IEnumerable<Categories> Get() 
+        {
+            return context.Categories.ToList();
+        }
     }
 }
