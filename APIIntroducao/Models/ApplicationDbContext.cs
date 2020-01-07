75ea81d5b9616b5e626c6c4e7cd072dc18c70ca7
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-01-07 11:21:50 -0300

    Added the Categories model to the DbContext file

--- APIIntroducao/Models/ApplicationDbContext.cs
+++ APIIntroducao/Models/ApplicationDbContext.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace APIIntroducao.Models
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
-        
+        /*
+            A classe ApplicationDbContext, deve receber o nome de todos os modelos criados,
+            o DbSet recebe o nome do modelo, você passa o nome que quer dar para a tabela
+            e os métodos que você quer passar para ela.
+        */
+        public DbSet<Categories> Categories { get; set; }
     }
 }
